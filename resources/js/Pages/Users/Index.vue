3d448ff91128b02365dcc40d5baa12fc579019d3
raga <ragashreeg@gmail.com>
2024-04-05 18:01:32 +0800

    initial setup

---
+++ resources/js/Pages/Users/Index.vue
@@ -0,0 +1,15 @@
+<template>
+   
+    <table>
+        <tr><th>User</th></tr>
+        <tr v-for="(user, id) in props.users" :v-bind:key="user.id">
+            <td>{{ user.name }}</td>
+        </tr>
+    </table>
+</template>
+
+<script setup>
+
+const props = defineProps(['users']);
+
+</script>
---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+import { createApp, h } from 'vue';
+// import { InertiaProgress } from '@inertiajs/progress';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+ 
+// InertiaProgress.init()
+
+createInertiaApp({
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+  setup({ el, App, props, plugin }) {
+    return createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el)
+  },
+});
