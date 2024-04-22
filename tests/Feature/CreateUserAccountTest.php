a7d6037a16dd8c545d50c1ed7793b60b696bba9d
ragashree1 <ragashreeg@gmail.com>
2024-04-22 11:34:25 +0800

    testing cicd pipeline

--- tests/Feature/CreateUserAccountTest.php
+++ tests/Feature/CreateUserAccountTest.php
@@ -23,26 +23,25 @@ class CreateUserAccountTest extends TestCase
         $this->seed(UserProfileSeeder::class);
     }
 
-    // public function test_system_admin_can_create_user_account(): void
-    // {
-    //     $user = User::factory()->create(['user_profile_id' => '1', 'password' => self::PASS]);
-    //     $this->actingAs($user);
+    public function test_system_admin_can_create_user_account(): void
+    {
+        $user = User::factory()->create(['user_profile_id' => '1', 'password' => self::PASS]);
+        $this->actingAs($user);
 
-    //     $response = $this->followingRedirects()->post('/users/store', [
-    //         'username' => 'abcde',
-    //         'first_name' => 'John',
-    //         'last_name' => 'Lee',
-    //         'email' => 'abcd@example.com',
-    //         'contact' => '12345678',
-    //         'dob' => Carbon::createFromFormat('Y-m-d', '2010-10-10'),
-    //     ]);
+        $response = $this->post('users', [
+            'username' => 'abcde',
+            'first_name' => 'John',
+            'last_name' => 'Lee',
+            'email' => 'abcd@example.com',
+            'contact' => '12345678',
+            'dob' => Carbon::createFromFormat('Y-m-d', '2010-10-10'),
+        ]);
 
-    //     // $response->assertDontSeeText('hi idiont');
-
-    //     // $this->assertDatabaseHas('users', [
-    //     //     'username' => 'abcde'
-    //     // ]);
-    // }
+        // dd($response);
+        $this->assertDatabaseHas('users', [
+            'username' => 'abcde'
+        ]);
+    }
 
 
 }
