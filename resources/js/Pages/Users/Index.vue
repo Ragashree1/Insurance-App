d9b35aa351f7c8c4aa794f6aedd92e3e87c606f5
ragashree1 <ragashreeg@gmail.com>
2024-04-27 18:29:14 +0800

    add search function

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -20,18 +20,7 @@ const showModal = ref(false);
 const showAssignRoleModal = ref(false);
 const search = ref(props.search);
 
-// watch(search, (search, prevVal) => {
-//     if (search == null || search == '') {
-//         router.get(route('users.index'));
-//     } else {
-//         debounce(() => {
-//             router.get(route('search-users', search));
-//             console.log('debug')
-//         }, 500)
-//     }
-// })
-
-const searchUsers = debounce(() => {
+const searchUser = debounce(() => {
     router.get(route('search-users', search.value));
 }, 500)
 
@@ -179,7 +168,7 @@ onMounted(() => {
                                                     d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                                             </svg>
                                         </div>
-                                        <input type="search" id="default-search" v-model="search" @input="searchUsers"
+                                        <input type="search" id="default-search" v-model="search" @input="searchUser"
                                             class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                             placeholder="Search" />
                                     </div>
