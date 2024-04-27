ac1d6b2becb38676278374fa30b8b36d8394a709
ragashree1 <ragashreeg@gmail.com>
2024-04-27 12:17:54 +0800

    add in test case for suspending user

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -195,12 +195,12 @@ onMounted(() => initFlowbite())
                                                     class="flex items-center px-5 py-2 border-b border-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600 hover:text-gray-900 dark:hover:text-white dark:border-gray-600">
                                                     <template v-if="user.status == 'active'">
                                                         <span class="text-sm font-medium hover:cursor-pointer"
-                                                            @click="suspendAccount(user)">Suspend
+                                                            @click="suspendAccount(user.id)">Suspend
                                                             User</span>
                                                     </template>
                                                     <template v-else>
                                                         <span class="text-sm font-medium hover:cursor-pointer"
-                                                            @click="activateAccount(user)">Activate
+                                                            @click="activateAccount(user.id)">Activate
                                                             User</span>
 
                                                     </template>
